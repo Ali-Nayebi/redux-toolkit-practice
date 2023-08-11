63f82ee1abd05ce98b17c9c0902bf9810deb92de
Ali Nayebi <elderekkoo@gmail.com>
2023-08-11 11:22:02 +0330

    modal component set up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CartContainer from "./components/CartContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
+import Modal from "./components/Modal";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
@@ -10,9 +11,11 @@ function App() {
 
   useEffect(() => {
     dispatch(calculateTotals());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cartItems]);
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
